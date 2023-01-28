41fa0d28cbfe0e731dd080404a516b8d89cda893
Matees <88842870+maytees@users.noreply.github.com>
2023-01-27 19:00:22 -0500

    finished prompts

---
+++ src/depts.ts
@@ -0,0 +1,5 @@
+export * as pupeteer from 'https://deno.land/x/puppeteer@16.2.0/mod.ts';
+
+export { Input, Confirm } from 'https://deno.land/x/cliffy@v0.25.7/prompt/mod.ts';
+
+export { colors } from "https://deno.land/x/cliffy@v0.25.7/ansi/mod.ts";
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { colors, Confirm, Input } from "./depts.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+await main();
+
+async function main() {
+  // Ask for info about CNAME and auth code
+  const host: string = await Input.prompt({
+    message: "What is the hostname?",
+    hint: "Example: example.domain.com",
+  });
+
+  const port: string = await Input.prompt({
+    message: "What is the port number?",
+    hint: "Example: 6183",
+  });
+
+  const authcode: string = await Input.prompt({
+    message: "What is the auth code for HE DNS",
+  });
+
+  console.log(
+    colors.bold.cyan(`Host: ${host}, port: ${port}, autcode: ${authcode}`),
+  );
+  if (!await Confirm.prompt("Is the info above correct?")) {
+    await main();
+    return;
+  }
 }
