b769ccc1bf9a874395fbb8137cb1c3bc46ced9d0
Matees <88842870+maytees@users.noreply.github.com>
2023-01-27 19:17:02 -0500

    fixed typo

--- src/depts.ts
+++ src/depts.ts
@@ -1,7 +1,7 @@
-export * as pupeteer from 'https://deno.land/x/puppeteer@16.2.0/mod.ts';
+export * as puppeteer from 'https://deno.land/x/puppeteer@16.2.0/mod.ts';
 
 export { Input, Confirm } from 'https://deno.land/x/cliffy@v0.25.7/prompt/mod.ts';
 
 export { colors } from "https://deno.land/x/cliffy@v0.25.7/ansi/mod.ts";
 
-export { config } from "https://deno.land/x/dotenv/mod.ts";
+export { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
